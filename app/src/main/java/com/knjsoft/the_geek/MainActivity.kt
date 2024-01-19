9137cb93bb23e49ff08ce68bb3e3408da6009bbc
KNJSoft <knjprod@gmail.com>
2024-01-19 21:00:54 +0100

    initialisation des activity

---
+++ app/src/main/java/com/knjsoft/the_geek/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.knjsoft.the_geek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
+
+lateinit var textinput:TextInputEditText;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+
+    }
+}
---
+++ app/src/main/java/com/knjsoft/the_geek/Start.kt
@@ -0,0 +1,20 @@
+package com.knjsoft.the_geek
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Start : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+        Handler(Looper.getMainLooper()).postDelayed({
+            Intent(this, Inscription::class.java).also {
+                startActivity(it)
+            }
+            finish()
+        }, 5000)
+    }
+}
