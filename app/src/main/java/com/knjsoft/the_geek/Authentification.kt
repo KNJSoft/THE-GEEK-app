3e9c3a64fdb4a5c11f7fe872c949016876241543
KNJSoft <knjprod@gmail.com>
2024-01-19 22:28:58 +0100

    ajout des activity

---
+++ app/src/main/java/com/knjsoft/the_geek/Authentification.kt
@@ -0,0 +1,11 @@
+package com.knjsoft.the_geek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Authentification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentification)
+    }
+}
