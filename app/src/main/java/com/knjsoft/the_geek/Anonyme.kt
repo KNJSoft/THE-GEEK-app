ce38f5cbf8507d7a442338f7580ff02628127d66
KNJSoft <knjprod@gmail.com>
2024-01-20 10:59:26 +0100

    home anonymous

--- app/src/main/java/com/knjsoft/the_geek/Anonyme.kt
+++ app/src/main/java/com/knjsoft/the_geek/Anonyme.kt
@@ -3,18 +3,15 @@ package com.knjsoft.the_geek
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.textfield.TextInputEditText
 
-lateinit var textinput:TextInputEditText;
-class MainActivity : AppCompatActivity() {
+class Anonyme : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
-
+        setContentView(R.layout.activity_anonyme)
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_home,menu)
+        menuInflater.inflate(R.menu.anonyme,menu)
         return super.onCreateOptionsMenu(menu)
     }
 }
